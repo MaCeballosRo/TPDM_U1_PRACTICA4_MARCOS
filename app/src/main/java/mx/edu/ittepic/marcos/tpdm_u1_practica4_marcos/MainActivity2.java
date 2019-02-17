4075a944e7a8099beb3d203bd3481e079f29791e
MaCeballosRo <47377947+MaCeballosRo@users.noreply.github.com>
2019-02-16 23:27:35 -0700

    Add files via upload

---
+++ app/src/main/java/mx/edu/ittepic/marcos/tpdm_u1_practica4_marcos/MainActivity2.java
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.marcos.tpdm_u1_practica4_marcos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
